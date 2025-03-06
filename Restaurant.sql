1bc308f0b9034d21f8e9184c631e287da26807f9
vagifaliyev1 <119289398+vagifaliyev1@users.noreply.github.com>
2025-03-07 00:57:46 +0400

    Update Restaurant.sql

--- Restaurant.sql
+++ Restaurant.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Menu;
 
 CREATE TABLE Customers(
   cstmr_id INT PRIMARY KEY,
@@ -24,3 +25,31 @@ START TRANSACTION;
 DELETE FROM Customers WHERE cstmr_id = 1001;
 
 ROLLBACK;
+
+CREATE TABLE Menu (
+   item_id INT PRIMARY KEY,
+   name_item VARCHAR(30) NOT NULL,
+   description TEXT,
+   price DECIMAL (10,2) NOT NULL, 
+   availability BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO Menu (item_id, name_item, description, price, availability)
+VALUES
+    (100, 'Cheeseburger', 'Juicy beef patty with melted cheese, lettuce, and tomato.', 7.95, TRUE),
+    (101, 'Margherita Pizza', 'Classic pizza with tomato sauce, mozzarella, and basil.', 10.50, TRUE),
+    (102, 'Chocolate Cake', 'Rich chocolate cake with creamy frosting.', 5.75, TRUE),
+    (103, 'Coca-Cola', 'Chilled 500ml bottle of Coca-Cola.', 2.50, TRUE),
+    (104, 'Pasta Carbonara', 'Creamy pasta with bacon and parmesan cheese.', 9.95, FALSE);
+
+CREATE TABLE MenuItems AS
+SELECT
+   item_id,
+   name_item,
+   price
+FROM Menu;
+SELECT * FROM MenuItems;
+
+SELECT * FROM Menu;
+
+
